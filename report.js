7ab1ee1a4f185146b03a754616ff0b41e1c8821a
Justin Rainbow <justin.rainbow@sheknows.com>
2011-07-22 10:20:25 -0700

    Initial commit

---
+++ report.js
@@ -0,0 +1,43 @@
+GLOBAL.DEBUG = true;
+
+var VERSION = '0.1',
+    // include all the modules
+    sys = require('sys'),
+    fs = require('fs'),
+    url = require('url'),
+    http = require('http'),
+    mongo = require('mongodb'),
+    querystring = require('querystring');
+
+var host = 'pe-macmini.local', port = mongo.Connection.DEFAULT_PORT,
+    
+    servers = new mongo.ReplSetServers([
+      new mongo.Server('pe-macmini.local', port, {}),
+      new mongo.Server('webdev.local', port, {}),
+      new mongo.Server('devslave.local', 27019, {})
+    ], { 'rs_name': 'mongoset' }),
+    db = new mongo.Db('boomerang', servers, { native_parser:true });
+
+db.open(function(err, db) {
+
+  db.collection('beacon', function(err, collection) {
+    collection.find(function(err, cursor) {
+      cursor.toArray(function(err, docs) { 
+        console.log("Found %s docs", docs.length);
+        docs.forEach(function(doc){
+          console.log("%s:  startup: %s, domloading: %s, finished: %s",
+            doc.test_site,
+            doc.nt_res_st - doc.nt_req_st,
+            doc.nt_domloading - doc.nt_req_st,
+            doc.nt_res_end - doc.nt_req_st
+          );
+        });
+          
+        db.close();
+          
+      });
+      
+    });
+  });
+  
+});
